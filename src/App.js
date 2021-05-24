d2194b8267ffda7da4396350e9a04b8b0979da2b
Debbie Brannon <debbie.s.brannon@gmail.com>
2021-05-23 19:08:22 -0500

    Index of quotes visible in the browser

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+console.log(process.env.NODE_ENV)
+let baseUrl = ''
+
+// more on React environment variables
+// https://create-react-app.dev/docs/adding-custom-environment-variables/
+if (process.env.NODE_ENV === 'development') {
+  baseUrl = 'http://localhost:3003'
+} else {
+  baseUrl = 'heroku url here'
+}
+
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      quotes: []
+    }
+    // if you do not use an arrow function for getQuotes()
+    // this.getQuotes = this.getQuotes.bind(this)
+  }
+
+  getQuotes = () => {
+    // fetch to the backend
+    fetch(baseUrl + "/quotes")
+    .then(res => { return res.json()
+    }).then(data => { 
+      this.setState({
+        quotes: data,
+      }) 
+     })
+    // take the data and call json() to convert
+    // then console.log the data
+    // fetch()
+  }
+
+
+  // component lifecycle flowchart
+  // https://levelup.gitconnected.com/componentdidmakesense-react-lifecycle-explanation-393dcb19e459
+  componentDidMount() {
+    this.getQuotes() 
+  }
+  
+  render () {
+    console.log(this.state.quotes)
+    return (
+      <div className="App">
+      <h1>Quotes</h1>
+      <table>
+        <tbody>
+          { this.state.quotes.map(quote => {
+            return (
+              <tr key={quote.id} >
+                <td> {quote.quote} </td>
+                <td> {quote.author} </td>
+                <td> {quote.likes} </td>
+              </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
+      </div>
+    );
+  }
 }
 
 export default App;
