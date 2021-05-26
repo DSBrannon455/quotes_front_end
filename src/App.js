e7e505d0c9db0297fea9c035d5e1e042f61d3e23
Debbie Brannon <debbie.s.brannon@gmail.com>
2021-05-26 16:35:21 -0500

    Can update likes

--- src/App.js
+++ src/App.js
@@ -74,8 +74,76 @@ class App extends Component {
       console.log('error: ', err)
     }  
   }
+
+  toggleRead = async (quote)=>{
+
+    const url = baseUrl + "/quotes/" + quote._id
+
+    try{
+
+      const response = await fetch(url, {
+        method: "PUT",
+        body: JSON.stringify({
+          read: !quote.read
+        }),
+        headers:{
+          'Content-Type': 'application/json'
+        }
+      })
+
+      if (response.status === 200){
+        const updatedQuote = await response.json()
+        console.log(updatedQuote)
+        
+        const index = this.state.quotes.findIndex(quote=> quote._id === updatedQuote.data._id )
+        const copyQuotes = [...this.state.quotes]
+        copyQuotes[index].read = updatedQuote.data.read
+
+        this.setState({
+          quotes: copyQuotes
+        })
+      }
+
+    }
+    catch(err){
+      console.log('error: ', err)
+    }
+
+  }
   
    
+addLike = async (quote) => {
+  const url = baseUrl + '/quotes/' + quote._id
+
+  try{
+
+    const response = await fetch( url , {
+      method: 'PUT',
+      body: JSON.stringify({
+        likes: quote.likes+1
+      }),
+      headers: {
+        'Content-Type' : 'application/json'
+      }
+    })
+    
+    if (response.status===200){
+      const updatedQuote = await response.json()
+      const findIndex = this.state.quotes.findIndex(quote => quote._id === updatedQuote.data._id)
+      const copyQuotes = [...this.state.quotes]
+      copyQuotes[findIndex].likes = updatedQuote.data.likes
+
+      this.setState({
+        quotes: copyQuotes
+      })
+    }
+  }
+  catch(err){
+    console.log('Error => ', err)
+  }
+
+}
+    
   // component lifecycle flowchart
   // https://levelup.gitconnected.com/componentdidmakesense-react-lifecycle-explanation-393dcb19e459
   componentDidMount() {
@@ -93,9 +161,17 @@ class App extends Component {
           { this.state.quotes.map(quote => {
             return (
               <tr key={quote._id} >
-                <td> {quote.quote} </td>
+                <td onDoubleClick={()=> this.toggleRead(quote)}
+                  className={quote.read
+                    ?
+                      'read'
+                    :
+                      null}
+
+                > {quote.quote} </td>
                 <td> {quote.author} </td>
                 <td> {quote.likes} </td>
+                <td onClick={()=>this.addLike(quote)}>Like</td>
                 <td onClick={()=> this.deleteQuote(quote._id)}>X</td>
               </tr>
               )
